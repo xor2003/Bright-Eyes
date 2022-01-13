1d71b5a69e06cb077814246b60b2774e909cc830
x0R <xorrezerv2@list.ru>
2022-01-13 21:53:54 +0300

    fix: interrupts if CPU_Cycles!=0

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -576,8 +576,10 @@ void CPU_Interrupt(Bitu num,Bitu type,Bitu oldeip) {
 		Segs.val[cs]=mem_readw(base+(num << 2)+2);
 		Segs.phys[cs]=Segs.val[cs]<<4;
 		cpu.code.big=false;
-//		printf("Want to execute interrupt %x\n",num);
+//		printf("Want to execute interrupt %x cs:IP=%x:%x\n",num,Segs.val[cs],reg_eip);
 		defered_custom_call=true;
+		CPU_CycleLeft=CPU_Cycles;
+		CPU_Cycles=0;
 		return;
 	} else {
 		/* Protected Mode Interrupt */
